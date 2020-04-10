18969b7a1bf0862cfdf8570b25fcf2e268aad9fe
okostroma <oxano4ka96@tut.by>
2020-04-10 23:12:02 +0300

    hw-2

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,27 @@ import Message from "./components/Message/Message";
 import Avatar from "./components/Avatar/Avatar";
 
 
-const App = () => {
-    return (
-        <div className="App">
-            <MyName/>
-            <Avatar/>
-            <Message/>
-        </div>
-    );
+class App extends React.Component {
+
+    qualities = [
+        {id:0, title: 'good-tempered', active: false},
+        {id:1, title: 'funny', active: true},
+        {id:2, title: 'responsible', active: false},
+        {id:3, title: 'kind', active: false}
+    ];
+
+
+    render() {
+
+
+        return (
+            <div className="App">
+                <MyName/>
+                <Avatar/>
+                <Message quality={this.qualities} />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -2,20 +2,34 @@ import React from 'react';
 import classes from './Message.module.css'
 
 
-const Message = () => {
-    return (
-        <div className={classes.message}>
-            <p className={classes.author}>Кострома Оксана</p>
-            <p className={classes.content}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit olore eu fugiat nulla pariatur.
-            </p>
-            <p className={classes.time}> {new Date().toLocaleTimeString('ru-RU', {
-                hour: '2-digit',
-                minute: '2-digit'
-            })}</p>
-        </div>
+class Message extends React.Component {
 
-    );
+
+
+
+    render() {
+        let addQualities = this.props.quality.map(quality => {
+            let activeClassName = quality.active ? classes.activeClass : '';
+            return <span key={quality.id} className={activeClassName}>{quality.title + ' | '}</span>
+        });
+
+        return (
+            <div className={classes.message}>
+                <p className={classes.author}>Кострома Оксана</p>
+                <p className={classes.content}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit olore eu fugiat nulla pariatur.
+                    Lorem ipsum dolor sit amet.
+                </p>
+                {addQualities}
+                <p className={classes.time}> {new Date().toLocaleTimeString('ru-RU', {
+                    hour: '2-digit',
+                    minute: '2-digit'
+                })}</p>
+
+            </div>
+
+        );
+    }
 }
 
 export default Message;
